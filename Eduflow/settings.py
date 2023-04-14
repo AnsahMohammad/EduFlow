708cfbb977a752f1586c55f95ed9d1289bb6e69b
praveen <cbsrirangapraveen.2004@gmail.com>
2023-04-14 13:10:40 +0530

    static settings added

--- Eduflow/settings.py
+++ Eduflow/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
