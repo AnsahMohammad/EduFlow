ba74fcbbb03b98e9b3a4ea604acd2da1502e24d7
AnsahMohammad <mohdansah10@gmail.com>
2023-04-15 14:44:28 +0530

    Added view for Teachers and subject[auto]

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 import json
 from django.http import HttpResponseRedirect,HttpResponse,Http404
 from django.urls import reverse
-from .models import Class
+from .models import Class, Teacher, Subject
 
 
 # Create your views here.
@@ -37,6 +37,50 @@ def parent(request):
     return HttpResponse("hello")
 
 def teacher(request):
+    if request.method == "POST":
+        teach_ID = request.POST.get('teacher_id')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        gender = request.POST.get('gender')
+        dob = request.POST.get('dob')
+        phone_no = request.POST.get('phone_no')
+        address = request.POST.get('address')
+        salary = request.POST.get('salary')
+        hire_date = request.POST.get('hire_date')
+        
+        subject_id = request.POST.get('subject_id')
+        # Extracting Subject Info
+        sub_info = subject_id.split("-")
+
+        class_id = request.POST.get('class_id')
+        # Retrieve the Class instance
+        cls = Class.objects.get(pk=class_id)
+
+        email = request.POST.get('email')
+
+        teacher = Teacher.objects.create(
+            teacher_id = teach_ID,
+            first_name = first_name,
+            last_name=last_name,
+            gender=gender,
+            dob=dob,
+            phone_no=phone_no,
+            address=address,
+            salary=salary,
+            hire_date=hire_date,
+            subject_id=sub_info[0],
+            class_id=cls,
+            email=email
+        )
+
+        subject = Subject.objects.create(
+                teacher_id = teacher,
+                subject_id = sub_info[0],
+                subject_name = sub_info[1],
+            )
+
+        return redirect('index')
+
     try:
         classes = Class.objects.all()
     except classes.DoesNotExist:
