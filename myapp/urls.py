1b8af9aac8413d028d65aefc711fe8f215bb2cbf
praveen <cbsrirangapraveen.2004@gmail.com>
2023-04-21 23:55:29 +0530

    resolved and errors and connected home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('student_add',views.add_student,name='add_student'),
     path('show',views.show,name='show'),
     path('edit_student/<str:pk>',views.edit_student,name='edit_student'),
-    # path('edit_teacher/<str:pk>',views.edit_teacher,name='edit_teacher'),
+    path('edit_teacher/<str:pk>',views.edit_teacher,name='edit_teacher'),
     path('fees',views.fees,name='fees'),
-    path('fee_details/<str:pk>',views.fee_details,name='fee_details'),,
+    path('fee_details/<str:pk>',views.fee_details,name='fee_details'),
     path('grades',views.show_grades,name='grades'),
     path('class_grades',views.class_grades,name='class_grades'),
-    path('class_grades',views.class_grades,name='class_grades')
+    path('student_grade',views.student_grade,name='student_grade')
 ]
